c65c9529fe1f9c53889ae90ef3573007713a3bbf
Luis Ricardo <luis-ls17@hotmail.com>
2019-08-11 02:05:12 -0500

    seccion ficha especialidad 10%

---
+++ application/views/ficha-especialidad.php
@@ -0,0 +1,34 @@
+<?php
+  defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="page-fichesp">
+	<section class="section-portada">
+		<div class="img-portada">
+			<div class="bg-portada"></div>
+		</div>
+		<div class="texto-portada">
+			<h1> <?php echo $fEspecialidad['nombre']; ?> </h1>
+		</div>
+	</section>
+	<div class="angle-separator-content">
+	  	<div class="angle-separator-up-style-2"></div>
+	</div>
+	<section class="section-describe">
+		
+	</section>
+	<section class="section-staff">
+		
+	</section>
+	<section class="section-servicios">
+		
+	</section>
+</div>
+<div class="angle-separator-content">
+  	<div class="angle-separator-bottom"></div>
+</div>
+<script type="text/javascript">
+
+  	$(document).ready(function() { 
+    
+	});
+</script>
