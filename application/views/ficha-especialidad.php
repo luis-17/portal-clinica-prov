4942ab8f4ebb34d785d357e1489de56efd994139
Luis Ricardo <luis-ls17@hotmail.com>
2019-08-16 21:49:12 -0500

    detalle especialidad 70%

--- application/views/ficha-especialidad.php
+++ application/views/ficha-especialidad.php
@@ -53,42 +53,59 @@
 			<button type="button" class="btn btn-primary"> RESERVA TU CITA </button>
 		</div>
 	</section>
-	<section class="section-servicios">
+	<section class="section-servicios container">
 		<div class="box-title">
-			Mas servicios para tí
+			<h2> Mas servicios para ti </h2>
 		</div>
 		<div class="box-content row">
 			<div class="box-item col-md-3">
-				<img src="<?php echo base_url().'assets/dinamic/servicio/serv-1.png'; ?>" alt="" />
-				<p> Ambulancia </p>
+				<a href="#">
+					<img src="<?php echo base_url().'assets/dinamic/servicio/serv-1.png'; ?>" alt="" />
+					<p> Ambulancia </p>
+				</a>
+				
 			</div>
 			<div class="box-item col-md-3">
-				<img src="<?php echo base_url().'assets/dinamic/servicio/serv-2.png'; ?>" alt="" />
-				<p> Emergencia 24 horas </p>
+				<a href="#">
+					<img src="<?php echo base_url().'assets/dinamic/servicio/serv-2.png'; ?>" alt="" />
+					<p> Emergencia 24 horas </p>
+				</a>
 			</div>
 			<div class="box-item col-md-3">
-				<img src="<?php echo base_url().'assets/dinamic/servicio/serv-3.png'; ?>" alt="" />
-				<p> Hospitalización </p>
+				<a href="#">
+					<img src="<?php echo base_url().'assets/dinamic/servicio/serv-3.png'; ?>" alt="" />
+					<p> Hospitalización </p>
+				</a>
 			</div>
 			<div class="box-item col-md-3">
-				<img src="<?php echo base_url().'assets/dinamic/servicio/serv-4.png'; ?>" alt="" />
-				<p> Centro Quirúrgico </p>
+				<a href="#">
+					<img src="<?php echo base_url().'assets/dinamic/servicio/serv-4.png'; ?>" alt="" />
+					<p> Centro Quirúrgico </p>
+				</a>
 			</div>
 			<div class="box-item col-md-3">
-				<img src="<?php echo base_url().'assets/dinamic/servicio/serv-5.png'; ?>" alt="" />
-				<p> Farmacia </p>
+				<a href="">
+					<img src="<?php echo base_url().'assets/dinamic/servicio/serv-5.png'; ?>" alt="" />
+					<p> Farmacia </p>	
+				</a>
 			</div>
 			<div class="box-item col-md-3">
-				<img src="<?php echo base_url().'assets/dinamic/servicio/serv-6.png'; ?>" alt="" />
-				<p> Laboratorio </p>
+				<a href="#">
+					<img src="<?php echo base_url().'assets/dinamic/servicio/serv-6.png'; ?>" alt="" />
+					<p> Laboratorio </p>
+				</a>
 			</div>
 			<div class="box-item col-md-3">
-				<img src="<?php echo base_url().'assets/dinamic/servicio/serv-7.png'; ?>" alt="" />
-				Departamento de Imágenes
+				<a href="#">
+					<img src="<?php echo base_url().'assets/dinamic/servicio/serv-7.png'; ?>" alt="" />
+					<p> Departamento de Imágenes </p> 
+				</a>
 			</div>
 			<div class="box-item col-md-3">
-				<img src="<?php echo base_url().'assets/dinamic/servicio/serv-8.png'; ?>" alt="" />
-				<p> Unidad de Cuidados Intensivos </p>
+				<a href="#">
+					<img src="<?php echo base_url().'assets/dinamic/servicio/serv-8.png'; ?>" alt="" />
+					<p> Unidad de Cuidados Intensivos </p> 
+				</a>
 			</div>
 		</div>
 	</section>
