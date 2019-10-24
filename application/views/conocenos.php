51637857f7291932a6812e178a87f5b691d5a5f9
Luis Ricardo <luis-ls17@hotmail.com>
2019-10-23 23:53:41 -0500

    seccion conocenos 100%

--- application/views/conocenos.php
+++ application/views/conocenos.php
@@ -18,33 +18,30 @@
 		<div class="box-background" style="background-image: url(<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/conocenos-ficha.png);"></div>
 		<div class="box-barra">
 			<div class="box-barra-1"></div>
-			<div class="box-barra-2"></div>
+			<div class="box-barra-2">
+				<div class="box-option box-option-mision selected" data-destino="mision">
+					<div class="box-icon">
+						<img class="si-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/mision-alt.png" />
+						<img class="no-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/mision.png" />
+					</div>
+				</div>
+				<div class="box-option box-option-vision" data-destino="vision">
+					<div class="box-icon">
+						<img class="si-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/vision-alt.png" />
+						<img class="no-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/vision.png" />
+					</div>
+				</div>
+				<div class="box-option box-option-valores" data-destino="valores">
+					<div class="box-icon">
+						<img class="si-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/valores-alt.png" />
+						<img class="no-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/valores.png" />
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="box-info">
-			<div class="box-options-icono">
-				<div class="box-option-mision">
-					<div class="box-icon">
-						ICONO
-					</div>
-				</div>
-				<div class="box-option-vision">
-					<div class="box-icon">
-						ICONO
-					</div>
-				</div>
-				<div class="box-option-valores">
-					<div class="box-icon">
-						ICONO
-					</div>
-				</div>
-				<!-- <div class="box-option-ubicacion">
-					<div class="box-icon">
-						ICONO
-					</div>
-				</div> -->
-			</div>
 			<div class="box-options-detail">
-				<div class="box-option-mision">
+				<div class="box-option box-option-mision">
 					<div class="box-title">
 						<h3>Misión</h3>
 						<small> Lorem ipsum Lorem ipsum</small>
@@ -53,7 +50,7 @@
 						<p>Brindar una atención de calidad a nuestros pacientes, transmitiendo calidez, confianza y seguridad.</p>
 					</div>
 				</div>
-				<div class="box-option-vision">
+				<div class="box-option box-option-vision">
 					<div class="box-title">
 						<h3>Visión</h3>
 						<small> Lorem ipsum Lorem ipsum</small>
@@ -62,10 +59,10 @@
 						<p>Ser reconocida como LA CLÍNICA de referencia en Lima, por nuestro modelo de atención humanizado y de calidad.</p>
 					</div>
 				</div>
-				<div class="box-option-valores">
+				<div class="box-option box-option-valores">
 					<div class="box-title">
 						<h3>Valores</h3>
-						<small> Lorem ipsum Lorem ipsum</small>
+						<small> Lorem ipsum Lorem ipsum </small>
 					</div>
 					<div class="box-parrafo">
 						<ul>
@@ -77,9 +74,6 @@
 						</ul>
 					</div>
 				</div>
-				<!-- <div class="box-option-ubicacion">
-					
-				</div> -->
 			</div>
 		</div>
 	</section>
@@ -87,3 +81,23 @@
 	  	<div class="angle-separator-bottom wow slideInDown slow"></div>
 	</div>
 </div>
+<script type="text/javascript">
+  	$(document).ready(function() {
+  		var objSelected = $('.box-barra-2 .box-option.selected');
+  		var destino = objSelected.attr('data-destino');
+  		var classDestino = 'box-option-' + destino;
+  		$('.box-options-detail .box-option.'+classDestino).show();
+  		$('.box-barra-2 .box-option').on('click', function() {
+  			var thes = $(this);
+  			$('.box-barra-2 .box-option').removeClass('selected');
+  			thes.addClass('selected');
+  			var destino = thes.attr('data-destino');
+  			var classDestino = 'box-option-' + destino;
+
+  			$('.box-options-detail .box-option').hide();
+  			// console.log(classDestino, 'classDestino');
+  			// console.log($('.box-options-detail .box-option.'+classDestino));
+  			$('.box-options-detail .box-option.'+classDestino).show();
+  		});
+  	});
+</script> 	
