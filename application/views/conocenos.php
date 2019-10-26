1edb698729eb6f5deba9f9a0a59f12f8df9ae69f
Luis Ricardo <luis-ls17@hotmail.com>
2019-10-26 10:50:28 -0500

    cambios en pagina conocenos

--- application/views/conocenos.php
+++ application/views/conocenos.php
@@ -15,25 +15,27 @@
 	  	<div class="angle-separator-up-style-2 wow slideInUp slow"></div>
 	</div>
 	<section class="section-conocenos">
-		<div class="box-background" style="background-image: url(<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/conocenos-ficha.png);"></div>
+		<div class="box-content-bg">
+			<div class="box-background" style="
+			background-image: linear-gradient(to top, rgba(213, 222, 229, 1) 0%, rgba(255, 255, 255, 0) 25%, rgba(255, 255, 255, 0) 75%, rgba(213, 222, 229, 1) 100%), 
+			url(<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/conocenos-ficha.png);">
+			</div>
+		</div>
 		<div class="box-barra">
 			<div class="box-barra-1"></div>
 			<div class="box-barra-2">
 				<div class="box-option box-option-mision selected" data-destino="mision">
 					<div class="box-icon">
-						<img class="si-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/mision-alt.png" />
 						<img class="no-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/mision.png" />
 					</div>
 				</div>
 				<div class="box-option box-option-vision" data-destino="vision">
 					<div class="box-icon">
-						<img class="si-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/vision-alt.png" />
 						<img class="no-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/vision.png" />
 					</div>
 				</div>
 				<div class="box-option box-option-valores" data-destino="valores">
 					<div class="box-icon">
-						<img class="si-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/valores-alt.png" />
 						<img class="no-alt" src="<?php echo URL_PREVIEW; ?>assets/dinamic/conocenos/iconos/valores.png" />
 					</div>
 				</div>
@@ -86,7 +88,7 @@
   		var objSelected = $('.box-barra-2 .box-option.selected');
   		var destino = objSelected.attr('data-destino');
   		var classDestino = 'box-option-' + destino;
-  		$('.box-options-detail .box-option.'+classDestino).show();
+  		$('.box-options-detail .box-option.'+classDestino).fadeIn(2000);
   		$('.box-barra-2 .box-option').on('click', function() {
   			var thes = $(this);
   			$('.box-barra-2 .box-option').removeClass('selected');
@@ -94,10 +96,23 @@
   			var destino = thes.attr('data-destino');
   			var classDestino = 'box-option-' + destino;
 
-  			$('.box-options-detail .box-option').hide();
-  			// console.log(classDestino, 'classDestino');
-  			// console.log($('.box-options-detail .box-option.'+classDestino));
-  			$('.box-options-detail .box-option.'+classDestino).show();
+  			$('.box-options-detail .box-option').hide().removeClass('animated bounceInRight delay-500ms');
+  			$('.box-options-detail .box-option.'+classDestino).fadeIn(2000).addClass('animated bounceInRight delay-500ms');
+
+  			// correr los iconos
+  			if (thes.hasClass('box-option-mision')) {
+  				$('.box-barra-2 .box-option.box-option-vision').css({left: '36%', top: '66%'});
+  				$('.box-barra-2 .box-option.box-option-valores').css({left: '-2%', top: '87%'});
+  			}
+  			if (thes.hasClass('box-option-vision')) {
+  				$('.box-barra-2 .box-option.box-option-mision').css({left: '36%', top: '24%'});
+  				$('.box-barra-2 .box-option.box-option-valores').css({left: '36%', top: '66%'});
+  			}
+  			if (thes.hasClass('box-option-valores')) {
+  				$('.box-barra-2 .box-option.box-option-mision').css({left: '0', top: '2%'});
+  				$('.box-barra-2 .box-option.box-option-vision').css({left: '36%', top: '24%'});
+  			}
+  			thes.css({left: '45%', top: '45%'});
   		});
   	});
 </script> 	
